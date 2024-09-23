5fbe9c0fea70b293f5a5734cd151fe21c439c207
Ian Setia <isetia@student13.cse.nd.edu>
2024-09-23 17:20:28 -0400

    HW03 - completed task 3.2 on checktests.py and added README.mdfor task 3.1

---
+++ hw/hw03/README.md
@@ -0,0 +1,4 @@
+Ian Setia
+isetia@nd.edu
+
+Task 3.1: We would want to filter first as it limits/reduces the number of elements our code would need to sort, thereby making the program run more efficiently.
--- hw/hw03/checktests.py
+++ hw/hw03/checktests.py
@@ -1,2 +1,35 @@
 #Ian Setia
 #isetia@nd.edu
+
+import argparse, requests, json
+
+def fetch_json(url):
+    response = requests.get(url)
+    data = response.json()
+    return data
+    
+def filter_json(data, month=5, year=2024, interface="eth0"):
+    filtered_data = []
+    for entry in data:
+        time_year = int(entry.get("timestamp")[:4])
+        time_month = int(entry.get("timestamp")[5:7])
+        if time_year == year and time_month == month and entry.get("interface") == interface:
+            filtered_data.append(entry)
+    return filtered_data
+
+def main():
+    parser = argparse.ArgumentParser(description="Fetch JSON file from given URL.")
+    parser.add_argument("url", type=str, help="URL to fetch the JSON file from.")
+
+    args = parser.parse_args()
+
+    json_data = fetch_json(args.url)
+    
+    if json_data is not None:
+        print(json.dumps(json_data, indent=4))
+
+    filtered = filter_json(json_data)
+    print(json.dumps(filtered, indent=4))
+
+if __name__ == "__main__":
+    main()
