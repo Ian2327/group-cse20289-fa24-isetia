d4235427e048c90af0484775c28bec368d29d6d1
Ian Setia <isetia@student13.cse.nd.edu>
2024-11-04 19:07:06 -0500

    HW07 - completed sf.sh and aa.sh to put all three other shell scripts together

---
+++ hw/hw07/aa.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+#set -x
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: sh aa.sh <archive_file> <bad_urls_csv>"
+	exit 1
+fi
+
+ARCHIVE_FILE=$1
+BAD_URLS_CSV=$2
+
+if [ ! -f $ARCHIVE_FILE ]; then
+	echo "The file $ARCHIVE_FILE does not exist."
+	exit 1
+fi
+
+if [ ! -f $BAD_URLS_CSV ]; then
+	echo "The file $BAD_URLS_CSV does not exist."
+	exit 1
+fi
+
+sh ae.sh $ARCHIVE_FILE > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+	echo "There was an error extracting the archive."
+	exit 1
+fi
+
+if [ ! -d archive ]; then
+	echo "Failed to extract: directory 'archive' was not found."
+	exit 1
+fi
+
+for FILE in archive/*; do
+	sh sbs.sh "$BAD_URLS_CSV" "$FILE" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		SBS_RESULTS=$(sh sbs.sh "$BAD_URLS_CSV" "$FILE" | tail -n 1)
+		if [ "$SBS_RESULTS" != "CLEAN" ]; then
+			echo "$SBS_RESULTS"
+			rm -rf archive
+			exit 0
+		fi
+	fi
+	sh sf.sh "$FILE" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		SF_RESULTS=$(sh sf.sh "$FILE" | tail -n 1)
+		if [ "$SF_RESULTS" != "CLEAN" ]; then
+			echo "$SF_RESULTS"
+			rm -rf archive
+			exit 0
+		fi
+	fi
+
+done
+
+rm -rf archive
+echo "CLEAN"
+
---
+++ hw/hw07/sf.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: sh sf.sh <file_to_check>"
+	exit 1
+fi
+
+FILE=$1
+
+if [ ! -f $FILE ]; then
+	echo "The file $FILE does not exist."
+	exit 1
+fi
+
+echo "Scanning for sensitive information"
+echo "File to scan: $FILE"
+
+if grep -q "\*SENSITIVE\*" "$FILE"; then
+	echo "SENSITIVE, MARKED SENSITIVE"
+	exit 0
+fi
+
+if grep -Eq '[0-9]{3}-[0-9]{2}-[0-9]{4}' "$FILE"; then
+	echo "SENSITIVE, SSN"
+	exit 0
+fi
+
+if grep -Eq '90[0-9]{7}' "$FILE"; then
+	echo "SENSITIVE, STUDENTID"
+	exit 0
+fi
+
+echo "CLEAN"
