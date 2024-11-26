31c57ae9f2c34db8e65fdb05720fa07286909c01
Ian Setia <isetia@student13.cse.nd.edu>
2024-11-26 12:08:04 -0500

    HW10 - completed shell script bb.sh for Task 4c

--- hw/hw09/README.md
+++ hw/hw09/README.md
@@ -6,6 +6,7 @@ isetia@nd.edu, alinare2@nd.edu
 - Uncommented and added print statements to see different variables and error messages
 - Ran redextract-debug using gdb to locate where code stops running
 - Found the error in 2500 in pcap-read.h (Ian)
+	- The value 2500 for defult read buffer variable was larger than the set maximum value (1500)
 - Ran redextract-debug using valgrind with --leak-check=full --show-leak-kinds=all -s to show all memory related errors and locations
 - Found malloc'ed and unfreed variable, theInfo.FileName, and updated main.c to free theInfo.FileName to resolve one valgrind error (Ian)
 - Set BigTable contents to NULL and freed BigTable to resolve valgrind error (Ian)
---
+++ hw/hw10/bb.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+CONFIG_FILE=".config"
+
+if [[ ! -f $CONFIG_FILE ]]; then
+	echo "Error: Config file $CONFIG_FILE cannot be found."
+	exit 1
+fi
+
+EXE=$(grep '^EXE' $CONFIG_FILE | cut -d '=' -f 2)
+HOST=$(grep '^HOST' $CONFIG_FILE | cut -d '=' -f 2)
+PORT=$(grep '^PORT' $CONFIG_FILE | cut -d '=' -f 2)
+
+if [[ ! -x $EXE ]]; then
+	echo "Error: Executable file name is missing in $CONFIG_FILE or is not executable"
+	exit 1
+fi
+
+if [[ -z $HOST ]]; then
+	echo "Error: Hostname is missing in $CONFIG_FILE"
+	exit 1
+fi 
+
+if [[ -z $PORT ]]; then 
+	echo "Error: Port is missing in $CONFIG_FILE"
+	exit 1
+fi
+
+if [[ $# -lt 1 ]]; then
+	echo "Usage: $0 [-query <filter_string>] <year> <month> <day> <hour>"
+	exit 1
+fi
+
+ARG1=$1
+if [[ $ARG1 == "-query" ]]; then
+	if [ $# -ne 6 ]; then
+		echo "Usage: $0 -query <filter_string> <year> <month> <day> <hour>"
+		exit 1
+	fi
+	FILTER_STRING=$2
+	YEAR=$3
+	MONTH=$4
+	DAY=$5
+	HOUR=$6
+	./$EXE "$HOST" "$PORT" -query "$FILTER_STRING" "$YEAR" "$MONTH" "$DAY" "$HOUR"
+else
+	if [ $# -ne 4 ]; then
+		echo "Usage: $0 <year> <month> <day> <hour>"
+		exit 1
+	fi
+	YEAR=$2
+	MONTH=$3
+	DAY=$4
+	HOUR=$5
+	./$EXE "$HOST" "$PORT" "$YEAR" "$MONTH" "$DAY" "$HOUR"
+fi
